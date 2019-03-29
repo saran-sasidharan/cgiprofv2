4942fe809c1196097e07173ac9685ebaf92c349c
Ubuntu <ubuntu@ip-172-31-29-67.ap-south-1.compute.internal>
2019-03-29 05:30:35 +0000

    Edits

--- jenkins/scripts/aws-deploy.sh
+++ jenkins/scripts/aws-deploy.sh
@@ -4,5 +4,5 @@ aws ecs register-task-definition --cli-input-json file://fargate-task.json --reg
 
 aws ecs list-task-definitions --region ap-south-1
 
-aws ecs create-service --region ap-south-1 --cluster ${CLUSTER_NAME} --service-name fargate-service --task-definition cgiprofessionalnetcoreservices --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0008f92ed5887c067],securityGroups=[sg-08fb93c325f5ae436],assignPublicIp=ENABLED}"
+aws ecs create-service --region ap-south-1 --cluster ${CLUSTER_NAME} --service-name fargate-service --task-definition cgiprofessionalnetcoreservices --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-053d3fc5429dc2b61],securityGroups=[sg-0504de6fac208870a],assignPublicIp=ENABLED}"
 
